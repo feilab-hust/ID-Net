95ecdac8b5599b724cd9b4eb19f651f5016eacde
hustlongbiao <94814159+hustlongbiao@users.noreply.github.com>
2021-12-01 19:35:31 +0800

    Update eval_all.py

--- eval_all.py
+++ eval_all.py
@@ -1,11 +1,4 @@
-# import tensorflow as tf
-import tensorflow.compat.v1 as tf
-from tensorflow.compat.v1 import ConfigProto
-from tensorflow.compat.v1 import InteractiveSession
-config = ConfigProto()
-config.gpu_options.allow_growth = True
-session = InteractiveSession(config=config)
-
+import tensorflow as tf
 import tensorlayer as tl
 import numpy as np
 import tifffile
